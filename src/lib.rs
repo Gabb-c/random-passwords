ae533d68cd77c2696138fc5d57fda4d37ed3221f
gabriel.cunha <gabriel.cunha@consulting-for.edenred.com>
2023-05-25 03:40:14 -0300

    feat: add tests

---
+++ src/lib.rs
@@ -0,0 +1,44 @@
+pub mod anagram {
+    use tracing::info;
+
+    pub fn get_anagrams(word: &str) -> Vec<String> {
+        let mut result = Vec::new();
+        let mut chars: Vec<char> = word.chars().collect();
+        let length = chars.len();
+        info!("generating all possible passwords");
+        generate_anagrams(length, &mut chars, &mut result);
+
+        result
+    }
+
+    fn generate_anagrams(length: usize, chars: &mut [char], result: &mut Vec<String>) {
+        if length == 1 {
+            result.push(chars.iter().collect());
+            return;
+        }
+
+        for i in 0..length {
+            generate_anagrams(length - 1, chars, result);
+            if length % 2 == 0 {
+                chars.swap(i, length - 1);
+            } else {
+                chars.swap(0, length - 1);
+            }
+        }
+
+        result.sort();
+        result.dedup()
+    }
+}
+
+#[cfg(test)]
+mod anagram_tests {
+    use super::anagram::get_anagrams;
+
+    #[test]
+    fn test_get_anagrams() {
+        let list = get_anagrams("test");
+
+        assert_eq!(12, list.len());
+    }
+}
